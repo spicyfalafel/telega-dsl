97d296da322278c4c2fd9b87e46e7772c293e4f2
spicyfalafel <285574@niuitmo.ru>
2024-04-01 01:14:02 +0300

    add check step

--- src/poc/core.clj
+++ src/poc/core.clj
@@ -7,6 +7,7 @@
    [poc.validation :as validation]
    [poc.example-group :as example]
    [poc.bot :as bot]
+   [poc.steps :as steps]
    [org.httpkit.client :as client]
    [org.httpkit.server :as hk-server]
    [malli.generator :as mg]))
@@ -19,60 +20,23 @@
 (defn dbdelete [path]
   (println "Deleting stub " path))
 
-(def config
-  {:timeout 10
-   :sleep 10000}) ;thread/sleep is in milliseconds
-
 #_(defmacro menu [items]
     `(for [item# ~items]
        {:label (:label item#)
         :on-select (fn [] (dbsave (:save-as item#) (:value item#)))}))
 
-(def no-id-step-prefix "no-id-step")
-
-
-(defn get-ids [steps]
-  (vec (map-indexed
-        (fn [idx s]
-          (if-let [id (:id s)]
-            id
-            (str no-id-step-prefix idx)))
-        steps)))
-
-(defn send-menu [id data menu]
-  (tbot/send-message bot/mybot id "todo send menu"))
-
-(defn call-message-fn [id data message]
-  (tbot/send-message bot/mybot id "todo send message fn")
-  )
-
-(defn handle-step [step id data]
-  (cond-> []
-    (string? (:message step))
-    (conj #(tbot/send-message bot/mybot id (:message step)))
-
-    (:message step)
-    (conj #(call-message-fn id data (:message step)))
-
-    (:menu step)
-    (conj #(send-menu id data (:menu step)))))
-
 (defn handle-command [command id data]
   (assert (= true (validation/validate-command command)) "wrong command")
-  (let [steps (:steps command)
-        _ (println "steps " steps)
-        ids (get-ids steps)
-        message (:message command)
+  (let [message (:message command)
         steps (:steps command)]
+    (println "message" message)
+    (println "seps" steps)
     (cond
       message
       (tbot/send-message bot/mybot id message)
 
       steps
-      (doseq [step steps]
-        (doseq [f (handle-step step id data)]
-          (f))))
-    ids))
+      (steps/handle-current-step steps id data))))
 
 (defn app [commands]
   (assert (map? commands) "expected commands to be map")
@@ -81,11 +45,16 @@
       (let [body (json/read-value (:body req) json/keyword-keys-object-mapper)
             data (-> body :message :text)
             id (-> body :message :chat :id)
-            ; TODO: find current step
-            commands-handled (mapv
-                               (fn [[c-name v]]
-                                 (handle-command (assoc v :name c-name) id data))
-                               commands)]
+            command-key
+            (when (str/starts-with? data "/")
+              (first (filterv #(= % (keyword (subs data 1)))
+                              (keys commands))))]
+        (println "command key" command-key)
+        (if command-key
+          (handle-command
+            (assoc (command-key commands) :name command-key)
+            id data)
+          (tbot/send-message bot/mybot id "no such command"))
         (println "\nid " id " data " data "\n")
         {:status  200
          :headers {"Content-Type" "text/html"}
@@ -109,7 +78,7 @@
 (comment
   (start-bot {:help {} :a {}})
 
-  (def ngrok-url "https://e97a-188-243-183-57.ngrok-free.app")
+  (def ngrok-url "https://b973-188-243-183-57.ngrok-free.app")
   (def token (System/getenv "BOT_TOKEN"))
 
   ;;https://api.telegram.org/bot{my_bot_token}/setWebhook?url={url_to_send_updates_to}
---
+++ src/poc/steps.clj
@@ -0,0 +1,92 @@
+(ns poc.steps
+  (:require
+   [telegrambot-lib.core :as tbot]
+   [malli.core :as m]
+   [clojure.string :as str]
+   [jsonista.core :as json]
+   [poc.validation :as validation]
+   [poc.example-group :as example]
+   [poc.bot :as bot]
+   [org.httpkit.client :as client]
+   [org.httpkit.server :as hk-server]
+   [malli.generator :as mg]))
+
+(set! *warn-on-reflection* true)
+
+(def no-id-step-prefix "no-id-step")
+
+(defn add-ids [steps]
+  (let [steps-vec (vec (map-indexed
+                        (fn [idx s]
+                          (if (:id s)
+                            s
+                            (assoc s :id (str no-id-step-prefix idx))))
+                        steps))]
+    (reduce
+     (fn [acc step]
+       (assoc acc (keyword (str (:id step))) step))
+     {}
+     steps-vec)))
+
+^:rct/test
+(comment
+  (add-ids [{:id 1 :hello 2} {:id 2 :hello 3} {:hello 4}])
+;=> {:1 {:id 1, :hello 2},
+;    :2 {:id 2, :hello 3},
+;    :no-id-step2 {:hello 4, :id "no-id-step2"}}
+  (add-ids []) ;=> {}
+  )
+
+(defn send-menu [id data menu]
+  (tbot/send-message bot/mybot id "todo send menu"))
+
+(defn call-message-fn [id data message]
+  (tbot/send-message bot/mybot id "todo send message fn"))
+
+(defn handle-step [step id data]
+  (let [fns (cond-> []
+              (string? (:message step))
+              (conj #(tbot/send-message bot/mybot id (:message step)))
+
+              (:message step)
+              (conj #(call-message-fn id data (:message step)))
+
+              (:menu step)
+              (conj #(send-menu id data (:menu step))))]
+    (doseq [f fns]
+      (f))))
+
+(def CURRENT_STEP (atom {}))
+
+(defn current-step [current-step-atom steps id]
+  (let [all-steps (keys steps)
+        last-step (get @current-step-atom id)]
+    (if last-step
+      (get steps (second (drop-while #(not= % last-step) all-steps)))
+      (get steps (first all-steps)))))
+
+(defn change-current-step [current-step-atom step id]
+  (swap! current-step-atom (fn [m] (assoc m id (:id step)))))
+
+^:rct/test
+(comment
+  ;; (def stp1 (atom {}))
+  ;; (def stps {:step1 {:hui 1 :id :step1}
+  ;;            :step2 {:hui 2 :id :step2}})
+  ;; (current-step stp1 stps 123) ;=> {:hui 1, :id :step1}
+  ;; (change-current-step stp1 {:hui 1, :id :step1} 123)
+  ;; (= @stp {123 :step1}) ;=> true
+  ;; (current-step stp stps 123) ;=> {:hui 2, :id :step2}
+  ;; (change-current-step stp {:hui 2, :id :step2} 123)
+  ;; (= @stp {123 :step2}) ;=> true
+  )
+
+(defn handle-current-step [steps id data]
+  (let [steps (add-ids steps)
+        current-step (current-step @CURRENT_STEP steps id)]
+    (println "current-step " current-step)
+    (handle-step current-step id data)
+    (println "handled current step")
+    (change-current-step CURRENT_STEP current-step id)))
+
+(com.mjdowney.rich-comment-tests/run-ns-tests! *ns*)
